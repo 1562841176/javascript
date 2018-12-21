b3a450df77933c4bee479337cbe70c91889fc9eb
1562841176 <1562841176@qq.com>
2018-12-21 15:52:37 +0800

    js代码移动位置

--- object/patternFactory.js
+++ object/patternFactory.js
@@ -1,7 +1,7 @@
 //工厂模式实际是一个包含参数的函数封装，调用后返回拥有参数和方法的对象，多次调用解决重复代码问题，但是存在对象识别的问题，同时也拥有构造函数模式相同的缺点
 
 function Fn(name,age){
-    var obj = ne Object();
+    var obj = new Object();
     obj.name = name;
     obj.age = age;
     obj.getName = function(){
